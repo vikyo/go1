995e8427d19071c49deff942c914fe38f71d232e
v0c03uq <vikash.chauhan@walmart.com>
2024-07-13 16:33:49 +0530

    78: refactor using structs

--- structs/app.go
+++ structs/app.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	firstNameInput := getUserData("Please enter your first name: ")
+	lastNameInput := getUserData("Please enter your last name: ")
+	birthdateInput := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	// ... do something awesome with that gathered data!
+	var appUser = user{
+		firstName: firstNameInput,
+		lastName:  lastNameInput,
+		birthdate: birthdateInput,
+		createdAt: time.Now(),
+	}
 
-	fmt.Println(firstName, lastName, birthdate)
+	outputUserDetails(appUser)
+}
+
+func outputUserDetails(u user) {
+	fmt.Println(u.firstName, u.lastName, u.birthdate, u.createdAt)
 }
 
 func getUserData(promptText string) string {
